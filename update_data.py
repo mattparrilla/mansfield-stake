cc34f3ea935b9d8479c90d388d5a2aa50b2e6bfd
Matthew Parrilla <matthew.parrilla@gmail.com>
2015-12-19 11:36:43 -0500

    Use null for current year beyond most recent reading

--- update_data.py
+++ update_data.py
@@ -6,7 +6,7 @@ import calendar
 
 
 def update_data():
-    r = requests.get("http://www.uvm.edu/~empact/data/gendateplot.php3?table=SummitStation&title=Mount+Mansfield+Summit+Station&xskip=7&xparam=Date&yparam=Depth&year%5B%5D=1954&year%5B%5D=1955&year%5B%5D=1956&year%5B%5D=1957&year%5B%5D=1958&year%5B%5D=1959&year%5B%5D=1960&year%5B%5D=1961&year%5B%5D=1962&year%5B%5D=1963&year%5B%5D=1964&year%5B%5D=1965&year%5B%5D=1966&year%5B%5D=1967&year%5B%5D=1968&year%5B%5D=1969&year%5B%5D=1970&year%5B%5D=1971&year%5B%5D=1972&year%5B%5D=1973&year%5B%5D=1974&year%5B%5D=1975&year%5B%5D=1976&year%5B%5D=1977&year%5B%5D=1978&year%5B%5D=1979&year%5B%5D=1980&year%5B%5D=1981&year%5B%5D=1982&year%5B%5D=1983&year%5B%5D=1984&year%5B%5D=1985&year%5B%5D=1986&year%5B%5D=1987&year%5B%5D=1988&year%5B%5D=1989&year%5B%5D=1990&year%5B%5D=1991&year%5B%5D=1992&year%5B%5D=1993&year%5B%5D=1994&year%5B%5D=1995&year%5B%5D=1996&year%5B%5D=1997&year%5B%5D=1998&year%5B%5D=1999&year%5B%5D=2000&year%5B%5D=2001&year%5B%5D=2002&year%5B%5D=2003&year%5B%5D=2004&year%5B%5D=2005&year%5B%5D=2006&year%5B%5D=2007&year%5B%5D=2008&year%5B%5D=2009&year%5B%5D=2010&year%5B%5D=2011&year%5B%5D=2012&year%5B%5D=2013&year%5B%5D=2014&year%5B%5D=2015&width=800&height=600&smooth=0&csv=1&totals=0")
+    r = requests.get("http://www.uvm.edu/~empact/data/gendateplot.php3?table=SummitStation&title=Mount+Mansfield+Summit+Station&xskip=7&xparam=Date&yparam=Depth&year%5B%5D=1954&year%5B%5D=1955&year%5B%5D=1956&year%5B%5D=1957&year%5B%5D=1958&year%5B%5D=1959&year%5B%5D=1960&year%5B%5D=1961&year%5B%5D=1962&year%5B%5D=1963&year%5B%5D=1964&year%5B%5D=1965&year%5B%5D=1966&year%5B%5D=1967&year%5B%5D=1968&year%5B%5D=1969&year%5B%5D=1970&year%5B%5D=1971&year%5B%5D=1972&year%5B%5D=1973&year%5B%5D=1974&year%5B%5D=1975&year%5B%5D=1976&year%5B%5D=1977&year%5B%5D=1978&year%5B%5D=1979&year%5B%5D=1980&year%5B%5D=1981&year%5B%5D=1982&year%5B%5D=1983&year%5B%5D=1984&year%5B%5D=1985&year%5B%5D=1986&year%5B%5D=1987&year%5B%5D=1988&year%5B%5D=1989&year%5B%5D=1990&year%5B%5D=1991&year%5B%5D=1992&year%5B%5D=1993&year%5B%5D=1994&year%5B%5D=1995&year%5B%5D=1996&year%5B%5D=1997&year%5B%5D=1998&year%5B%5D=1999&year%5B%5D=2000&year%5B%5D=2001&year%5B%5D=2002&year%5B%5D=2003&year%5B%5D=2004&year%5B%5D=2005&year%5B%5D=2006&year%5B%5D=2007&year%5B%5D=2008&year%5B%5D=2009&year%5B%5D=2010&year%5B%5D=2011&year%5B%5D=2012&year%5B%5D=2013&year%5B%5D=2014&year%5B%5D=2015&year%5B%5D=2016&width=800&height=600&smooth=0&csv=1&totals=0")
 
     data = csv.reader(StringIO(r.text), delimiter=',')
     snow_csv = [l for l in data]
@@ -21,6 +21,9 @@ def update_data():
     # snowdepth table will populate our csv
     snowdepth_table = [header_row]
     season_list = []
+    last_reading = snow_csv[-2][0].split('-')
+    last_reading_month = last_reading[1]
+    last_reading_day = last_reading[2]
 
     # parse CSV input
     for row in snow_csv[1:-1]:
@@ -59,7 +62,6 @@ def update_data():
 
     # parse every date in every year, even if no measurement
     for i, year in enumerate(snowdepth_table):
-
         # Skip first row (headers)
         if not i:
             continue
@@ -80,6 +82,7 @@ def update_data():
             if last_depth == 'null':
                 snowdepth_table[i][j] = 'null'
 
+                continue
             # the source data is ugly, sometimes large blocks of dates get
             # skipped, sometimes there are 0s where there should be no
             # measurement, sometimes there are impossible measurements
@@ -113,6 +116,21 @@ def update_data():
             # In 1956 there's an extra zero (120 instead of 12) for depth
             elif depth - last_depth > 100:
                 snowdepth_table[i][j] = last_depth
+
+            elif ((i == len(snowdepth_table) - 1)
+                    and (month == int(last_reading_month))
+                    and day >= int(last_reading_day)):
+                print 'testing'
+                snowdepth_table[i][j] = 'null'
+
+            # else:
+            #     print '- - - - \n'
+            #     print i, len(snowdepth_table) - 1
+            #     print i == len(snowdepth_table) - 1
+            #     print month, last_reading_month
+            #     print month == int(last_reading_month)
+            #     print day, last_reading_day
+            #     print day >= int(last_reading_day)
 
             # set last_depth for next loop
             last_depth = snowdepth_table[i][j]
