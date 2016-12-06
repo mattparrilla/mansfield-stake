41d67090014a7c769e4f342aa604eb34305142a7
Matthew Parrilla <matthew.parrilla@gmail.com>
2016-12-05 19:09:55 -0500

    Rip out non-functioning responsive code

--- app/src/components/YearlyTrend.jsx
+++ app/src/components/YearlyTrend.jsx
@@ -4,58 +4,57 @@ import * as d3 from 'd3';
 import './YearlyTrend.css';
 
 class YearlyTrend extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  componentDidUpdate() {
-    const { data = [] } = this.props;
+  componentDidMount() {
     const svg = d3.select(this.chart);
     const margin = {
       top: 20, right: 80, bottom: 30, left: 50,
     };
+
     const width = this.props.width - margin.left - margin.right;
     const height = this.props.height - margin.top - margin.bottom;
-    const g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`);
 
-    const x = d3.scaleTime().range([0, width]);
-    const y = d3.scaleLinear().range([height, 0]);
-    const z = d3.scaleOrdinal(d3.schemeCategory10);
+    this.g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`);
 
-    const line = d3.line()
+    this.x = d3.scaleTime().range([0, width]);
+    this.y = d3.scaleLinear().range([height, 0]);
+    this.z = d3.scaleOrdinal(d3.schemeCategory10);
+
+    this.line = d3.line()
       .curve(d3.curveBasis)
-      .x(d => x(d.date))
-      .y(d => y(d.snowDepth));
+      .x(d => this.x(d.date))
+      .y(d => this.y(d.snowDepth));
 
-    x.domain([
-      d3.min(data, season => d3.min(season.values, date => date.date)),
-      d3.max(data, season => d3.max(season.values, date => date.date)),
-    ]);
-
-    y.domain([
-      d3.min(data, season => d3.min(season.values, date => date.snowDepth)),
-      d3.max(data, season => d3.max(season.values, date => date.snowDepth)),
-    ]);
-
-    z.domain(data.map(c => c.season));
-
-    g.append('g')
+    this.g.append('g')
       .attr('class', 'axis axis--x')
       .attr('transform', `translate(0,${height})`)
-      .call(d3.axisBottom(x).tickFormat(d3.timeFormat('%B')));
+      .call(d3.axisBottom(this.x).tickFormat(d3.timeFormat('%B')));
 
-    g.append('g')
+    this.g.append('g')
       .attr('class', 'axis axis--y')
-      .call(d3.axisLeft(y))
+      .call(d3.axisLeft(this.y))
     .append('text')
       .attr('transform', 'rotate(-90)')
       .attr('y', 6)
       .attr('dy', '0.71em')
       .attr('fill', '#000')
       .text('Snow Depth, inches');
+  }
 
-    const season = g.selectAll('.season')
+  componentDidUpdate() {
+    const { data = [] } = this.props;
+    this.x.domain([
+      d3.min(data, season => d3.min(season.values, date => date.date)),
+      d3.max(data, season => d3.max(season.values, date => date.date)),
+    ]);
+
+    this.y.domain([
+      d3.min(data, season => d3.min(season.values, date => date.snowDepth)),
+      d3.max(data, season => d3.max(season.values, date => date.snowDepth)),
+    ]);
+
+    this.z.domain(data.map(c => c.season));
+
+    const season = this.g.selectAll('.season')
       .data(data)
       .enter().append('g')
         .attr('class', 'season');
@@ -64,18 +63,18 @@ class YearlyTrend extends Component {
 
     season.append('path')
       .attr('class', 'line')
-      .attr('d', d => line(d.values));
+      .attr('d', d => this.line(d.values));
   }
 
   render() {
+    const { width, height } = this.props;
     return (
       <svg
         className="yearly-trend"
         ref={(ref) => { this.chart = ref; }}
-        width={this.props.width}
-        height={this.props.height}
+        width={width}
+        height={height}
       />
-
     );
   }
 }
--- app/src/components/YearlyTrendContainer.jsx
+++ app/src/components/YearlyTrendContainer.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { csv, timeParse } from 'd3';
 import YearlyTrend from './YearlyTrend';
-import ResponsiveWrapper from './ResponsiveWrapper';
 
 // TODO: get data from backend shaped this way
 const transformRow = (season) => {
@@ -34,12 +33,14 @@ class YearlyTrendContainer extends Component {
 
   render() {
     return (
-      <ResponsiveWrapper>
+      <div>
         <YearlyTrend
           key="snowDepth"
           data={this.state.data}
+          width={900}
+          height={500}
         />
-      </ResponsiveWrapper>
+      </div>
     );
   }
 }
