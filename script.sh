b8fa1e512c4a2f7b453b3fd4f3b118528898649f
Matthew Parrilla <matthew.parrilla@gmail.com>
2016-12-19 18:44:58 -0500

    Updates

--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd /home/mattparrilla/git/mansfield-stake && git pull && source venv/bin/activate && /home/mattparrilla/git/mansfield-stake/venv/bin/python2.7 update_data.py && git commit -am "Update data" && git push && deactivate
