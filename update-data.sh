b109a14f413b42f64e41616730cb9d6bbe4edf69
Matt Parrilla <matthew.parrilla@gmail.com>
2017-01-22 16:36:25 +0000

    update data by cron

--- update-data.sh
+++ update-data.sh
@@ -2,10 +2,12 @@
 . /home/mattparrilla/.bash_profile
 
 cd /home/mattparrilla/git/mansfield-stake \
-&& git checkout gh-pages
 && git pull \
 && source data/venv/bin/activate \
 && /home/mattparrilla/git/mansfield-stake/data/venv/bin/python2.7 data/update_data.py \
-&& git commit -am "cron update of data"
+&& deactivate \
+&& git commit -am "update data by cron" \
+&& git checkout gh-pages \
+&& git checkout master public/snowdepth.csv > snowdepth.csv \
+&& git commit -am "update data by cron" \
 && git push
-&& deactivate
