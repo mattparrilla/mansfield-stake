0fa56b0372dcce5337b620c71e2df2db58159ea4
Matt Parrilla <matthew.parrilla@gmail.com>
2017-01-22 16:27:32 +0000

    Updating webfaction script

--- update-data.sh
+++ update-data.sh
@@ -2,8 +2,10 @@
 . /home/mattparrilla/.bash_profile
 
 cd /home/mattparrilla/git/mansfield-stake \
+&& git checkout gh-pages
 && git pull \
 && source data/venv/bin/activate \
 && /home/mattparrilla/git/mansfield-stake/data/venv/bin/python2.7 data/update_data.py \
-&& /home/mattparrilla/.nvm/versions/node/v6.9.2/bin/npm run build \
+&& git commit -am "cron update of data"
+&& git push
 && deactivate
