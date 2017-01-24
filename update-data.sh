cd24b41f109fbd240c2a4c6e22bbd88022bfad27
Matt Parrilla <matthew.parrilla@gmail.com>
2017-01-24 19:54:02 +0000

    update data by cron

--- update-data.sh
+++ update-data.sh
@@ -2,6 +2,7 @@
 . /home/mattparrilla/.bash_profile
 
 cd /home/mattparrilla/git/mansfield-stake \
+&& git checkout master \
 && git pull \
 && source data/venv/bin/activate \
 && /home/mattparrilla/git/mansfield-stake/data/venv/bin/python2.7 data/update_data.py \
