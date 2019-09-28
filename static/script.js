acf931afda5162a14eea432c62b49d714b3f3f0c
Matthew Parrilla <matthew.parrilla@gmail.com>
2019-09-28 16:37:35 -0400

    Add wind speed chart

--- static/script.js
+++ static/script.js
@@ -217,7 +217,7 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   // Line chart with hover based on https://bl.ocks.org/larsenmtl/e3b8b7c2ca4787f77d78f58d41c3da91
-  const initTemperatureChart = () => {
+  const initPrev10Charts = (metrics) => {
     // const margin = {
     //   top: 20,
     //   right: 80,
@@ -225,13 +225,16 @@ document.addEventListener('DOMContentLoaded', () => {
     //   left: 50,
     // };
     // const width = 900 - margin.left - margin.right;
-    const height = 300 - margin.top - margin.bottom;
+    const height = (300 * metrics.length);
 
     const x = d3.scaleTime()
       .range([0, width - margin.left - margin.right]);
 
-    const y = d3.scaleLinear()
-      .range([height, 0]);
+    const y = d3.scaleLinear();
+
+    const line = d3.line()
+      .x((d) => x(d.timestamp))
+      .curve(d3.curveBasis);
 
     const xAxis = d3.axisBottom()
       .scale(x);
@@ -239,21 +242,48 @@ document.addEventListener('DOMContentLoaded', () => {
     const yAxis = d3.axisLeft()
       .scale(y);
 
-    const line = d3.line()
-      .x((d) => x(d.timestamp))
-      .y((d) => y(d.temperature))
-      .curve(d3.curveBasis);
-
-    const svg = d3.select('#prev_10_charts')
+    d3.select('#prev_10_charts')
       .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .select('#temperature')
-        .attr('transform', `translate(${margin.left},${margin.top})`);
+      .attr('height', height + margin.top + margin.bottom);
 
     const transformDate = row => ({
       ...row,
       timestamp: new Date(row.timestamp)
     });
+
+    const addChart = (data, key, order) => {
+      const chartHeight = 300;
+      const g = d3.select('#prev_10_charts')
+        .select(`#${key}`)
+          .attr('transform', `translate(${margin.left},${chartHeight * order + margin.top})`);
+
+      y.range([chartHeight, 0]);
+
+      const values = data.map(d => parseInt(d[key], 10));
+
+      x.domain(d3.extent(data, (d) => d.timestamp));
+      y.domain([
+        key === 'wind_speed' ? 0 : d3.min(values) - 2,
+        d3.max(values) + 2,
+      ]);
+
+      line.y((d) => y(d[key]));
+
+      g.append('g')
+        .attr('class', 'y axis')
+        .call(yAxis);
+
+      g.append('path')
+          .attr('class', 'line')
+          .attr('d', line(data))
+          .style('fill-opacity', 0)
+          .style('stroke', '#333');
+
+      g.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', `translate(0,${chartHeight})`)
+        .call(xAxis);
+    };
 
     // Download data, filter it, and render chart
     d3.csv(
@@ -261,126 +291,11 @@ document.addEventListener('DOMContentLoaded', () => {
       transformDate,
       unfilteredData => {
         const data = filterToLast10(unfilteredData);
-        const temperatures = data.map(d => d.temperature);
-
-        x.domain(d3.extent(data, (d) => d.timestamp));
-        y.domain([
-          d3.min(temperatures) - 2,
-          d3.max(temperatures),
-        ]);
-
-        svg.append('g')
-          .attr('class', 'x axis')
-          .attr('transform', `translate(0,${height})`)
-          .call(xAxis);
-
-        svg.append('g')
-          .attr('class', 'y axis')
-          .call(yAxis)
-          .append('text')
-          .attr('transform', 'rotate(-90)')
-          .attr('y', 6)
-          .attr('dy', '.71em')
-          .style('text-anchor', 'end')
-          .text('Temperature (ºF)');
-
-        const tempLineColor = '#333';
-        svg.append('path')
-            .attr('class', 'line')
-            .attr('d', line(data))
-            .style('fill-opacity', 0)
-            .style('stroke', tempLineColor);
-
-        // const mouseG = svg.append('g')
-        //   .attr('class', 'mouse-over-effects');
-
-        // mouseG.append('path') // this is the black vertical line to follow mouse
-        //   .attr('class', 'mouse-line')
-        //   .style('stroke', 'black')
-        //   .style('stroke-width', '1px')
-        //   .style('opacity', '0');
-
-        // const lines = document.getElementsByClassName('line');
-
-        // const mousePerLine = mouseG.selectAll('.mouse-per-line')
-        //   .data(data)
-        //   .enter()
-        //   .append('g')
-        //   .attr('class', 'mouse-per-line');
-
-        // mousePerLine.append('circle')
-        //   .attr('r', 7)
-        //   .style('stroke', tempLineColor)
-        //   .style('fill', 'none')
-        //   .style('stroke-width', '1px')
-        //   .style('opacity', '0');
-
-        // mousePerLine.append('text')
-        //   .attr('transform', 'translate(10,3)');
-
-        // mouseG.append('svg:rect') // append a rect to catch mouse movements on canvas
-        //   .attr('width', width) // can't catch mouse events on a g element
-        //   .attr('height', height)
-        //   .attr('fill', 'none')
-        //   .attr('pointer-events', 'all')
-        //   .on('mouseout', () => { // on mouse out hide line, circles and text
-        //     d3.select('.mouse-line')
-        //       .style('opacity', '0');
-        //     d3.selectAll('.mouse-per-line circle')
-        //       .style('opacity', '0');
-        //     d3.selectAll('.mouse-per-line text')
-        //       .style('opacity', '0');
-        //   })
-        //   .on('mouseover', () => { // on mouse in show line, circles and text
-        //     d3.select('.mouse-line')
-        //       .style('opacity', '1');
-        //     d3.selectAll('.mouse-per-line circle')
-        //       .style('opacity', '1');
-        //     d3.selectAll('.mouse-per-line text')
-        //       .style('opacity', '1');
-        //   })
-        //   .on('mousemove', () => { // mouse moving over canvas
-        //     const mouse = d3.mouse(this);
-        //     d3.select('.mouse-line')
-        //       .attr('d', () => {
-        //         let d = `M${mouse[0]},${height}`;
-        //         d += ` ${mouse[0]},${0}`;
-        //         return d;
-        //       });
-
-        //     d3.selectAll('.mouse-per-line')
-        //       .attr('transform', (_, i) => {
-        //         console.log(width / mouse[0]);
-        //         const xDate = x.invert(mouse[0]);
-        //         const bisect = d3.bisector((d) => d.timestamp).right;
-        //         bisect(data, xDate);
-
-        //         let beginning = 0;
-        //         let end = lines[i].getTotalLength();
-        //         let target = null;
-        //         let pos;
-
-        //         while (true) {
-        //           target = Math.floor((beginning + end) / 2);
-        //           pos = lines[i].getPointAtLength(target);
-        //           if ((target === end || target === beginning) && pos.x !== mouse[0]) {
-        //             break;
-        //           }
-        //           if (pos.x > mouse[0]) end = target;
-        //           else if (pos.x < mouse[0]) beginning = target;
-        //           else break; // position found
-        //         }
-
-        //         d3.select(this).select('text')
-        //           .text(y.invert(pos.y).toFixed(2));
-
-        //         return `translate(${mouse[0]},${pos.y})`;
-        //       });
-        //   });
+        metrics.forEach((metric, i) => addChart(data, metric, i));
       }
     );
   };
 
   initSnowDepthChart();
-  initTemperatureChart();
+  initPrev10Charts(['temperature', 'wind_speed']);
 });
